0431dcdc2c6c6bc8323c4943a5572da30abba842
Lee DeBoom <lee@kicksite.net>
2018-08-30 21:46:04 -0500

    allowing environment variable to not be set for consumers of gem when testing

--- lib/kicksite_svc_client.rb
+++ lib/kicksite_svc_client.rb
@@ -1,7 +1,10 @@
 Dir["#{File.dirname(__FILE__)}/kicksite_svc_client/**/*.rb"].each {|file| require file }
 
 module KicksiteSvcClient
-  raise 'Set KICKSITE_SVC_URL environment variable to utilize this gem' if ENV['KICKSITE_SVC_URL'].blank?
-  raise 'Set ADMIN_USER_NAME environment variable to utilize this gem' if ENV['ADMIN_USER_NAME'].blank?
-  raise 'Set ADMIN_PASSWORD environment variable to utilize this gem' if ENV['ADMIN_PASSWORD'].blank?
+  # Note: Anyone consuming this should mock the actual REST calls in their tests
+  unless ENV['RAILS_ENV'] == 'test'
+    raise 'Set KICKSITE_SVC_URL environment variable to utilize this gem' if ENV['KICKSITE_SVC_URL'].blank?
+    raise 'Set ADMIN_USER_NAME environment variable to utilize this gem' if ENV['ADMIN_USER_NAME'].blank?
+    raise 'Set ADMIN_PASSWORD environment variable to utilize this gem' if ENV['ADMIN_PASSWORD'].blank?
+  end
 end
--- spec/kicksite_svc_client_spec.rb
+++ spec/kicksite_svc_client_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe KicksiteSvcClient do
   it "has a version number" do
     expect(KicksiteSvcClient::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
