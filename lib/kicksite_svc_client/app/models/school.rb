9a6fe1d139c3f02824f2c9694cd70a27826f1c15
Lee DeBoom <lee@kicksite.net>
2019-02-20 13:22:28 -0600

    DEV-2247

--- lib/kicksite_svc_client/app/models/school.rb
+++ lib/kicksite_svc_client/app/models/school.rb
@@ -4,6 +4,10 @@ require_relative '../helpers/no_svc_object'
 # REST resources specific to Schools
 class School < KicksiteSvcBasicAuth
   class Logo < NoSvcObject; end
+  class Statistic < NoSvcObject; end
+
+  GROWTH_STATISTIC_TYPE = 'growth'.freeze
+  STUDENTS_STATISTIC_GROUP = 'students'.freeze
 
   def logo
     payload = get(:logo)
@@ -28,5 +32,10 @@ class School < KicksiteSvcBasicAuth
   # @return [PaginatedCollection] Collection of students associated with school
   def students(options = {})
     Schools::Student.find(:all, options.deep_merge({ params: { school_id: self.id } }))
+  end
+
+  def statistic(group, type)
+    payload = KicksiteSvcBearerAuth.get("schools/#{self.id}/stats/#{type}/#{group}")
+    School::Statistic.new(payload) if payload.present?
   end
 end
