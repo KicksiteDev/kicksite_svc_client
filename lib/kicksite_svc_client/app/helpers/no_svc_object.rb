1c91e95f2a5d1a12447132b81580897678dc8696
Lee DeBoom <lee@kicksite.net>
2019-03-29 11:06:15 -0500

    DEV-2249

--- lib/kicksite_svc_client/app/helpers/no_svc_object.rb
+++ lib/kicksite_svc_client/app/helpers/no_svc_object.rb
@@ -1,7 +1,16 @@
 # An object that acts sort of similar to an ActiveResource object
 # but without all the extras ActiveResource gives that doesn't exist for this.
 class NoSvcObject
+  BASE_DATETIME_KEYS = %w[
+    updated_at
+    created_at
+  ].freeze
+
   def initialize(payload)
+    BASE_DATETIME_KEYS.each do |key|
+      payload[key] = to_datetime(payload[key])
+    end
+
     payload.each do |key, value|
       define_instance_variable(key, value)
       define_getter(key)
@@ -31,5 +40,11 @@ class NoSvcObject
     define_singleton_method("#{key}=") do |value|
       instance_variable_set("@#{key}", value)
     end
+  end
+
+  protected
+
+  def to_datetime(datetime_string)
+    datetime_string.present? ? Time.parse(datetime_string) : datetime_string
   end
 end
