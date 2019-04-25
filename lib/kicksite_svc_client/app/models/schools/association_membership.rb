f7aa74993111508330635fa462e45d017bdcb010
Lee DeBoom <lee@kicksite.net>
2019-04-25 10:17:06 -0500

    DEV-2341

--- lib/kicksite_svc_client/app/models/schools/association_membership.rb
+++ lib/kicksite_svc_client/app/models/schools/association_membership.rb
@@ -1,5 +1,6 @@
 require_relative '../../helpers/kicksite_svc_bearer_auth'
 require_relative '../../helpers/paginated_collection'
+require_relative 'membership'
 
 module Schools
   # REST resources specific to Association Memberships at a given school
