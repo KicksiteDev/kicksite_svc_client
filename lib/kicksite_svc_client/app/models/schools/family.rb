76cadc5b9cba63d7db7358773cf69326514261b2
Nate <lander@kicksite.net>
2019-04-26 10:01:46 -0500

    small change to family automations

--- lib/kicksite_svc_client/app/models/schools/family.rb
+++ lib/kicksite_svc_client/app/models/schools/family.rb
@@ -4,6 +4,7 @@ module Schools
   # REST resources specific to Families at a given school
   class Family < KicksiteSvcBearerAuth
     self.prefix = '/v1/schools/:school_id/'
+    self.collection_parser = PaginatedCollection
 
     def automations(options = {})
       opt = options.dup
