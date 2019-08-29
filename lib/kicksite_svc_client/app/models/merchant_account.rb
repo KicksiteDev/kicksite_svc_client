d7c202f18c2fbcd293e21d505d7d55aaf73b9d60
Lee DeBoom <lee@kicksite.net>
2019-08-29 14:47:44 -0500

    forgot to implement save on merchant account

--- lib/kicksite_svc_client/app/models/merchant_account.rb
+++ lib/kicksite_svc_client/app/models/merchant_account.rb
@@ -1,4 +1,19 @@
 # Represents a School's MerchantAccount
 class MerchantAccount < NoSvcObject
   USA_EPAY_TYPE = 'UsaEpayMA'.freeze
+
+  def save
+    begin
+      save!
+    rescue StandardError
+      return false
+    end
+
+    true
+  end
+
+  def save!
+    KicksiteSvcBasicAuth.put("#{business_entity_type.downcase.pluralize}/#{business_entity_id}/merchant_account",
+                             to_hash)
+  end
 end
