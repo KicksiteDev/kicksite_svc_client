8c7af27c62a2459a60e75363f98406ab98fdf5e7
Lee DeBoom <lee@kicksite.net>
2020-01-22 13:03:17 -0600

    DEV-3437

---
+++ lib/kicksite_svc_client/app/models/schools/bizbuilder/landing_page.rb
@@ -0,0 +1,22 @@
+module Schools
+  module Bizbuilder
+    # A lead capture form thing
+    class LandingPage < KicksiteSvcBearerAuth
+      self.prefix = '/v1/schools/:school_id/bizbuilder/'
+      self.collection_parser = PaginatedCollection
+
+      class Submission < NoSvcObject; end
+
+      def submit(payload)
+        post(:submissions, payload: payload)
+      end
+
+      def submissions(options = {})
+        payload = get(:submissions, options)
+        PaginatedCollection.new(payload.map do |submission|
+          Schools::Bizbuilder::LandingPage::Submission.new(submission, true)
+        end)
+      end
+    end
+  end
+end
