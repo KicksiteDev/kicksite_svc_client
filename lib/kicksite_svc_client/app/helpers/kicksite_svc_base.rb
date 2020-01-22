16887f504cd6f640047018c36dc24c9925619f97
Lee DeBoom <lee@kicksite.net>
2020-01-22 16:22:40 -0600

    DEV-3437

--- lib/kicksite_svc_client/app/helpers/kicksite_svc_base.rb
+++ lib/kicksite_svc_client/app/helpers/kicksite_svc_base.rb
@@ -4,6 +4,8 @@ require 'activeresource-response'
 # Our base that primarily just defines the kicksite-svc base url
 class KicksiteSvcBase < ActiveResource::Base
   self.site = "#{ENV['KICKSITE_SVC_URL']}/v1"
+  self.collection_name = ''
+  self.element_name = ''
 
   BASE_DATETIME_KEYS = %w[
     updated_at
