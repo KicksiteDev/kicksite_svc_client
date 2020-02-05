f3a4340268b96fcdb5ee312096278932a1a916df
Jamie Little <jamie@jamielittle.org>
2020-02-05 16:00:42 -0600

    DEV-3508 Landing Page Submissions

--- lib/kicksite_svc_client/app/models/schools/bizbuilder/landing_page.rb
+++ lib/kicksite_svc_client/app/models/schools/bizbuilder/landing_page.rb
@@ -4,6 +4,15 @@ module Schools
     class LandingPage < KicksiteSvcBearerAuth
       self.prefix = '/v1/schools/:school_id/bizbuilder/'
       self.collection_parser = PaginatedCollection
+
+      class Submission < NoSvcObject; end
+
+      def submissions(options = {})
+        payload = get(:submissions, options)
+        PaginatedCollection.new(payload.map do |submission|
+                                  Schools::Bizbuilder::LandingPage::Submission.new(submission, true)
+                                end)
+      end
     end
   end
 end
---
+++ spec/models/schools/bizbuilder/landing_page_submissions_spec.rb
@@ -0,0 +1,29 @@
+require 'httparty'
+
+RSpec.describe Schools::Bizbuilder::LandingPage do
+  let(:school_id) { 119 }
+  let(:landing_page_id) { 1 }
+
+  before do
+    options = {
+      body: {
+        login: ENV['ADMIN_USER_NAME'],
+        password: ENV['ADMIN_PASSWORD'],
+        context: {
+          type: 'School',
+          id: school_id
+        }
+      }
+    }
+
+    new_user_session_url = "#{ENV['KICKSITE_AUTH_URL']}/v1/users/new/sessions"
+    token = HTTParty.post(new_user_session_url, options)['token']
+    KicksiteSvcBearerAuth.connection.bearer_token = token
+  end
+
+  it 'successfully returns all landing page submissions for a landing page' do
+    pending('waiting for submissions on kicksite-staging')
+    landing_page = Schools::Bizbuilder::LandingPage.find(landing_page_id, params: { school_id: school_id })
+    landing_page.submissions(school_id: school_id, landing_page_id: landing_page_id)
+  end
+end
