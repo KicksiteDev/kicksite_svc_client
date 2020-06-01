5ddec6595f101cc5b4257924f4e0c317986b9dc2
Nate <lander@kicksite.net>
2020-06-01 16:16:25 -0500

    update progression_level.rb

--- lib/kicksite_svc_client/app/models/schools/students/progression_level.rb
+++ lib/kicksite_svc_client/app/models/schools/students/progression_level.rb
@@ -1,9 +1,19 @@
 module Schools
   module Students
-    # REST resources specific to Progression Levels at a given school
+    # REST resources specific to Progression Levels (ranks) for given student
     class ProgressionLevel < KicksiteSvcBearerAuth
-      self.prefix = '/v1/schools/:school_id/students/'
+      self.prefix = '/v1/schools/:school_id/students/:student_id/'
       self.collection_parser = PaginatedCollection
+
+      def initialize(attributes = {}, persisted = false)
+        if persisted
+          APPOINTMENT_DATETIME_KEYS.each do |key|
+            attributes[key] = to_datetime(attributes[key])
+          end
+        end
+
+        super(attributes, persisted)
+      end
     end
   end
 end
