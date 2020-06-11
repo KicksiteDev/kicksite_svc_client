8c7ba6100fd1c8fd002da31e7198775bb99bf988
Lee DeBoom <lee@kicksite.net>
2020-06-11 14:11:34 -0500

    DEV-3903

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,12 +2,12 @@ RSpec.describe Person do
   let(:person_id) { 10_678 }
 
   it 'successfully returns a person' do
-    person = Person.find(person_id)
+    person = Kicksite::Person.find(person_id)
     expect(person.id).to eq person_id
   end
 
   it 'successfully returns a photo' do
-    person = Person.find(person_id)
+    person = Kicksite::Person.find(person_id)
     expect(person.photo).to_not be nil
   end
 end
