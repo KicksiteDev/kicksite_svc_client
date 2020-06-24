8574665ec1cc5f7f70f089fdbf4c71329127743d
Lee DeBoom <lee@kicksite.net>
2020-06-24 10:33:28 -0500

    DEV-3842: As an API consumer outside Kicksite, I need the ability to interact with several endpoints using my auth credentials

--- lib/kicksite_svc_client/app/helpers/kicksite_svc_basic_auth.rb
+++ lib/kicksite_svc_client/app/helpers/kicksite_svc_basic_auth.rb
@@ -1,7 +1,8 @@
 # Our base that requires basic authentication
 class KicksiteSvcBasicAuth < KicksiteSvcBase
-  self.user = ENV['ADMIN_USER_NAME']
-  self.password = ENV['ADMIN_PASSWORD']
+  connection.auth_type = :bearer
+  KicksiteSvcBearerAuth.connection.bearer_token =
+    Base64.strict_encode64("#{ENV['ADMIN_USER_NAME']}:#{ENV['ADMIN_PASSWORD']}")
 
   self.collection_name = ''
   self.element_name = ''
