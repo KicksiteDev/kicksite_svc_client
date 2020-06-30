fd6f3de73243b5e803561e04502d4f937d9ed0d1
Lee DeBoom <lee@kicksite.net>
2020-06-30 12:26:59 -0500

    FIX LEE'S HORRIBLE LAME EXCUSE FOR BEING A DEVELOPER

--- lib/kicksite_svc_client/app/helpers/kicksite_svc_basic_auth.rb
+++ lib/kicksite_svc_client/app/helpers/kicksite_svc_basic_auth.rb
@@ -1,8 +1,7 @@
 # Our base that requires basic authentication
 class KicksiteSvcBasicAuth < KicksiteSvcBase
-  connection.auth_type = :bearer
-  KicksiteSvcBearerAuth.connection.bearer_token =
-    Base64.strict_encode64("#{ENV['ADMIN_USER_NAME']}:#{ENV['ADMIN_PASSWORD']}")
+  self.user = ENV['ADMIN_USER_NAME']
+  self.password = ENV['ADMIN_PASSWORD']
 
   self.collection_name = ''
   self.element_name = ''
