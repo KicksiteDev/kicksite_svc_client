cbc3342a1c8f6fe29ee98c0c93c706a115cffd5b
Lee DeBoom <lee@kicksite.net>
2020-07-09 15:46:00 -0500

    DEV-4425: Investigate and introduce methods of caching to help improve TTFB (#60)

--- lib/kicksite_svc_client/app/helpers/no_svc_object.rb
+++ lib/kicksite_svc_client/app/helpers/no_svc_object.rb
@@ -21,7 +21,17 @@ module Kicksite
 
     def to_hash
       hash = {}
-      instance_variables.each { |var| hash[var.to_s.delete('@')] = instance_variable_get(var) }
+      instance_variables.each do |var|
+        get_var = instance_variable_get(var)
+        hash[var.to_s.delete('@')] = if get_var.present? && get_var.respond_to?(:to_hash)
+                                       get_var.to_hash
+                                     elsif get_var.present? && get_var.respond_to?(:attributes)
+                                       get_var.attributes
+                                     else
+                                       get_var
+                                     end
+      end
+
       hash
     end
 
@@ -43,10 +53,7 @@ module Kicksite
       end
     end
 
-    # rubocop:disable Metrics/AbcSize
-    # rubocop:disable Metrics/MethodLength
-
-    def recursive_define(key, value)
+    def recursive_define(key, value) # rubocop:disable Metrics/AbcSize
       if value.is_a?(Hash)
         define_instance_variable(key, Kicksite::NoSvcObject.new(value))
         define_setter(key, Kicksite::NoSvcObject.new(value))
@@ -61,9 +68,6 @@ module Kicksite
 
       define_getter(key, value)
     end
-
-    # rubocop:enable Metrics/AbcSize
-    # rubocop:enable Metrics/MethodLength
 
     def define_instance_variable(key, value)
       instance_variable_set("@#{key}", value)
