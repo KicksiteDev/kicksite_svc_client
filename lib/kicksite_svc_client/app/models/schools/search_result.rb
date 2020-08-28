82a5a07bf715645a634c05598c4bac37b321a546
Lee DeBoom <lee@kicksite.net>
2020-08-28 10:19:18 -0500

    DEV-4677: Pulled when searching "Buddy" , filtering "Event"

--- lib/kicksite_svc_client/app/models/schools/search_result.rb
+++ lib/kicksite_svc_client/app/models/schools/search_result.rb
@@ -4,7 +4,6 @@ module Kicksite
       STUDENT_TYPE = 'Student'.freeze
       PROSPECT_TYPE = 'Prospect'.freeze
       FAMILY_TYPE = 'Family'.freeze
-      EVENT_TYPE = 'Event'.freeze
       INVOICE_TYPE = 'Invoice'.freeze
     end
   end
