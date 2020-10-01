be09d29c16cebf231376a631f6214e1b728f6753
Jeff Wallace <jeffrey.wallace81@gmail.com>
2020-10-01 09:12:00 -0500

    DEV-4561 - Use the kicksite paginated collection parser for Student Agreements

--- lib/kicksite_svc_client/app/models/schools/students/agreement.rb
+++ lib/kicksite_svc_client/app/models/schools/students/agreement.rb
@@ -4,6 +4,7 @@ module Kicksite
       # REST resources specific to Agreements associated with given Student
       class Agreement < KicksiteSvcBearerAuth
         self.prefix = '/v1/schools/:school_id/students/:student_id/'
+        self.collection_parser = Kicksite::PaginatedCollection
 
         ACTIVE_FILTER = 'active'.freeze
       end
