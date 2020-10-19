b00768bd8a32d80151c81c898e4832338f24d50b
Jeff Wallace <jeffrey.wallace81@gmail.com>
2020-10-19 09:45:30 -0500

    DEV-4880 - Adds "date_signed" sort by value for Prospect/Student Agreements

--- lib/kicksite_svc_client/app/models/schools/prospects/agreement.rb
+++ lib/kicksite_svc_client/app/models/schools/prospects/agreement.rb
@@ -6,9 +6,10 @@ module Kicksite
         self.prefix = '/v1/schools/:school_id/prospects/:prospect_id/'
         self.collection_parser = Kicksite::PaginatedCollection
 
-        CREATED_AT_SORT_BY = 'created_at'.freeze
-        NAME_SORT_BY       = 'name'.freeze
-        STATUS_SORT_BY     = 'status'.freeze
+        CREATED_AT_SORT_BY  = 'created_at'.freeze
+        DATE_SIGNED_SORT_BY = 'date_signed'.freeze
+        NAME_SORT_BY        = 'name'.freeze
+        STATUS_SORT_BY      = 'status'.freeze
       end
     end
   end
