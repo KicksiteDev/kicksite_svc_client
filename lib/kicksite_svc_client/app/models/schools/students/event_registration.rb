593a52e21e631e55e963d955677fcffbe94b8826
nlc <33323778+nlc@users.noreply.github.com>
2020-11-19 13:32:00 -0600

    DEV-4939 As a user, I need to view the status of events in Profiles. (#74)
    * add event_registration concept
    
    * rubocop
    
    Co-authored-by: Nate <lander@kicksite.net>
    Co-authored-by: Jeffrey Wallace <jw81@users.noreply.github.com>

---
+++ lib/kicksite_svc_client/app/models/schools/students/event_registration.rb
@@ -0,0 +1,11 @@
+module Kicksite
+  module Schools
+    module Students
+      # REST resources specific to Events associated with given Student
+      class EventRegistration < KicksiteSvcBearerAuth
+        self.prefix = '/v1/schools/:school_id/students/:student_id/'
+        self.collection_parser = Kicksite::PaginatedCollection
+      end
+    end
+  end
+end
