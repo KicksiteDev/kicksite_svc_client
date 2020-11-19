aa7c90dfdad108d02226278aaf9a5276adce6898
Nate <lander@kicksite.net>
2020-11-19 09:15:43 -0600

    rubocop

--- lib/kicksite_svc_client.rb
+++ lib/kicksite_svc_client.rb
@@ -24,7 +24,7 @@ Dir["#{File.dirname(__FILE__)}/kicksite_svc_client/app/models/**/*.rb"].sort.eac
 
 # Entry to REST api model definitions
 module KicksiteSvcClient
-  # Note: Anyone consuming this should mock the actual REST calls in their tests
+  # NOTE: Anyone consuming this should mock the actual REST calls in their tests
   unless ENV['RAILS_ENV'] == 'test'
     raise 'Set KICKSITE_SVC_URL environment variable to utilize this gem' if ENV['KICKSITE_SVC_URL'].blank?
     raise 'Set ADMIN_USER_NAME environment variable to utilize this gem' if ENV['ADMIN_USER_NAME'].blank?
