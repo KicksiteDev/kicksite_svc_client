8504392aa8028cb607f2ce9cc5e37651a19bfe9f
Ryan Troxler <ryan@kicksite.net>
2020-11-23 14:30:52 -0600

    Add it to the right comments

--- lib/kicksite_svc_client/app/models/schools/people/comments.rb
+++ lib/kicksite_svc_client/app/models/schools/people/comments.rb
@@ -4,6 +4,7 @@ module Kicksite
       # REST resources specific to Programs associated with given person
       class Comments < KicksiteSvcBearerAuth
         self.prefix = '/v1/schools/:school_id/people/:person_id/'
+        self.collection_parser = Kicksite::PaginatedCollection
       end
     end
   end
