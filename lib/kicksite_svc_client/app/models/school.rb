69bc1e9970a4e3b712807f2d5a10568b158b9809
Ryan Troxler <rtroxler@rednovalabs.com>
2020-12-16 15:39:16 -0600

    Define AttendanceAwardSettings on school (#79)
    * al;skdjfk;lajf
    
    * ajklsdjfkasld[:wq
    [:w]]
    
    * ala;ksdjf
    
    * Iaslkdfjasdf[:]
    
    * Ojea;oijd;lkasjdf
    
    * alk;sjdf
    
    * jalskdjf
    
    * alskjdf
    
    * aklsdjf
    
    * Don't need this
    
    * Extract this into it's own file
    
    * iakdsljfklasdjfklajsdf
    
    * Iajskldjfal;ksdghfk;lasdjfkl;ajsd;klfj
    
    * rubocop

--- lib/kicksite_svc_client/app/models/school.rb
+++ lib/kicksite_svc_client/app/models/school.rb
@@ -15,6 +15,21 @@ module Kicksite
 
     class Tag < Kicksite::NoSvcObject; end
 
+    # Accesses the attendance_award_settings off a school.
+    # Not backed by any SvcObject, this is essentially just a nested hash on School
+    # (using terrible text column serialization). We want to define getter methods
+    # to define each of bronze/silver/gold, but the values of each of those should remain
+    # a hash, so we can access each program IDs corresponding setting.
+    class AttendanceAwardSettings
+      attr_accessor :attendance_award_bronze, :attendance_award_silver, :attendance_award_gold
+
+      def initialize(payload = {}, _persisted = false)
+        @attendance_award_bronze = payload['attendance_award_bronze']
+        @attendance_award_silver = payload['attendance_award_silver']
+        @attendance_award_gold = payload['attendance_award_gold']
+      end
+    end
+
     SCHOOL_DATETIME_KEYS = %w[
       subscription_plan_status_date
       subscription_plan_selected_at
