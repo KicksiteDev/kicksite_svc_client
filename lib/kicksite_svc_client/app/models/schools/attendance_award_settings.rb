1e69cfa82cfffa0ceff60d841e9d867b9b14045c
Ryan Troxler <ryan@kicksite.net>
2020-12-16 13:35:43 -0600

    iakdsljfklasdjfklajsdf

--- lib/kicksite_svc_client/app/models/schools/attendance_award_settings.rb
+++ lib/kicksite_svc_client/app/models/schools/attendance_award_settings.rb
@@ -1,6 +1,10 @@
 module Kicksite
-  module Schools
-    # REST resources specific to tasks at a given school
+  module School
+    # Accesses the attendance_award_settings off a school.
+    # Not backed by any SvcObject, this is essentially just a nested hash on School
+    # (using terrible text column serialization). We want to define getter methods
+    # to define each of bronze/silver/gold, but the values of each of those should remain
+    # a hash, so we can access each program IDs corresponding setting.
     class AttendanceAwardSettings
       attr_accessor :attendance_award_bronze, :attendance_award_silver, :attendance_award_gold
       def initialize(payload = {}, persisted = false)
