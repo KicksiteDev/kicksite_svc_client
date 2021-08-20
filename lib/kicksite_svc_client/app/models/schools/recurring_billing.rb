beb634f79570f13834cfdd421accebfffd6dd8a6
Donald Lee <7713872+dhcL@users.noreply.github.com>
2021-08-20 10:22:52 -0500

    Add name sort by for recurring billing

--- lib/kicksite_svc_client/app/models/schools/recurring_billing.rb
+++ lib/kicksite_svc_client/app/models/schools/recurring_billing.rb
@@ -16,9 +16,11 @@ module Kicksite
       ACTIVE_FILTER = 'active'.freeze
       ACTIVE_NOVAULT_FILTER = 'active-novault'.freeze
       ACTIVE_UNPAID_FILTER = 'active-unpaid'.freeze
-      INACTIVE_UNPAID_FILTER = 'inactive-unpaid'.freeze
-      PAID_THROUGH_SORT_BY = 'paid_through'.freeze
       COMPLETED_FILTER = 'completed'.freeze
+      INACTIVE_UNPAID_FILTER = 'inactive-unpaid'.freeze
+
+      NAME_SORT_BY = 'name'.freeze
+      PAID_THROUGH_SORT_BY = 'paid_through'.freeze
 
       def initialize(attributes = {}, persisted = false)
         if persisted
