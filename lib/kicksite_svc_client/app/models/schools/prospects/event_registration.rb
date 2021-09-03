5fde58bf2c618840c2d108b84325739e71ee9af2
MSILycanthropy <53885212+MSILycanthropy@users.noreply.github.com>
2021-09-03 10:43:59 -0500

    Hopefully making prospects have event_registration

---
+++ lib/kicksite_svc_client/app/models/schools/prospects/event_registration.rb
@@ -0,0 +1,11 @@
+module Kicksite
+  module Schools
+    module Prospects
+      # REST resources specific to Events associated with given Student
+      class EventRegistration < KicksiteSvcBearerAuth
+        self.prefix = '/v1/schools/:school_id/prospects/:student_id/'
+        self.collection_parser = Kicksite::PaginatedCollection
+      end
+    end
+  end
+end
