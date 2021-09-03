5f0c7dcccfb89a63b2ee0e030ed4a48b854991b4
MSILycanthropy <53885212+MSILycanthropy@users.noreply.github.com>
2021-09-03 11:17:40 -0500

    Fixing the event registrations for prospects?

--- lib/kicksite_svc_client/app/models/schools/prospects/event_registration.rb
+++ lib/kicksite_svc_client/app/models/schools/prospects/event_registration.rb
@@ -3,7 +3,7 @@ module Kicksite
     module Prospects
       # REST resources specific to Events associated with given Student
       class EventRegistration < KicksiteSvcBearerAuth
-        self.prefix = '/v1/schools/:school_id/prospects/:student_id/'
+        self.prefix = '/v1/schools/:school_id/prospects/:prospect_id/'
         self.collection_parser = Kicksite::PaginatedCollection
       end
     end
